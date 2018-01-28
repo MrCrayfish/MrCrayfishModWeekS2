1affc940ff437a1bfab86354ff9fbc59aa5c09b6
MrCrayfish <mrcrayfish@hotmail.com>
2018-01-29 10:02:42 +1030

    Created template

---
+++ src/main/java/com/mrcrayfish/template/Reference.java
@@ -0,0 +1,12 @@
+package com.mrcrayfish.template;
+
+/**
+ * Author: MrCrayfish
+ */
+public class Reference
+{
+    public static final String MOD_ID = "template";
+    public static final String MOD_NAME = "Template TemplateMod";
+    public static final String MOD_VERSION = "1.0";
+    public static final String MOD_COMPATIBILITY = "[1.12.2]";
+}

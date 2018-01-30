c3c482bb001c6867cf88a25e60c1f1165f688cea
MrCrayfish <mrcrayfish@hotmail.com>
2018-01-31 09:45:58 +1030

    Reconfigured for today's mod

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -1,12 +1,12 @@
-package com.mrcrayfish.template;
+package com.mrcrayfish.vehicle;
 
 /**
  * Author: MrCrayfish
  */
 public class Reference
 {
-    public static final String MOD_ID = "template";
-    public static final String MOD_NAME = "Template TemplateMod";
+    public static final String MOD_ID = "vehicle";
+    public static final String MOD_NAME = "Vehicle Mod";
     public static final String MOD_VERSION = "1.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
 }
--- src/main/java/com/mrcrayfish/vehicle/VehicleMod.java
+++ src/main/java/com/mrcrayfish/vehicle/VehicleMod.java
@@ -1,4 +1,4 @@
-package com.mrcrayfish.template;
+package com.mrcrayfish.vehicle;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  * Author: MrCrayfish
  */
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MOD_COMPATIBILITY)
-public class TemplateMod
+public class VehicleMod
 {
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event)

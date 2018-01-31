4398a230d7883da748dc5169b0a796d3a38566cb
MrCrayfish <mrcrayfish@hotmail.com>
2018-02-01 09:54:59 +1030

    Created Vehicle Mod

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -9,4 +9,7 @@ public class Reference
     public static final String MOD_NAME = "Vehicle Mod";
     public static final String MOD_VERSION = "1.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
+
+    public static final String PROXY_CLIENT = "com.mrcrayfish.vehicle.proxy.ClientProxy";
+    public static final String PROXY_SERVER = "com.mrcrayfish.vehicle.proxy.ServerProxy";
 }
--- src/main/java/com/mrcrayfish/vehicle/VehicleMod.java
+++ src/main/java/com/mrcrayfish/vehicle/VehicleMod.java
@@ -1,7 +1,13 @@
 package com.mrcrayfish.vehicle;
 
+import com.mrcrayfish.vehicle.entity.EntityATV;
+import com.mrcrayfish.vehicle.init.RegistrationHandler;
+import com.mrcrayfish.vehicle.proxy.Proxy;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 /**
  * Author: MrCrayfish
@@ -9,9 +15,16 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MOD_COMPATIBILITY)
 public class VehicleMod
 {
+    @SidedProxy(clientSide = Reference.PROXY_CLIENT, serverSide = Reference.PROXY_SERVER)
+    public static Proxy proxy;
+
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event)
     {
+        RegistrationHandler.init();
 
+        EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID, "vehicle_atv"), EntityATV.class, "atv", 0, this, 64, 1, true);
+
+        proxy.preInit();
     }
 }
